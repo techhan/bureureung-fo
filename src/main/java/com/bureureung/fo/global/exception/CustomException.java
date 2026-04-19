14aa5ec13f01196591157b2e3fe5e9749944e708
techhan <devhan9117@gmail.com>
2026-04-19 12:21:27 +0900

    chore: 전역 예외 처리 구조 작성
    - ErrorCode enum 관리
    - CustomException으로 비즈니스 예외 표준화
    - GlobalExceptionHandler에서 @RestControllerAdvice로 예외 일괄 처리
    - 검증 실패, 잘못된 HTTP 메서드, 예상치 못한 서버 에러 핸들러 추가
    - 로그 레벨 분리 (예상된 에러: warn, 예상치 못한 에러: error)

---
+++ src/main/java/com/bureureung/fo/global/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.bureureung.fo.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public CustomException(final ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
