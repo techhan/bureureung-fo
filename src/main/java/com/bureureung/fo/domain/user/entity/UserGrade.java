bdfeeb3c18717699080c757b661d46e8ab289c2a
techhan <devhan9117@gmail.com>
2026-04-20 09:04:12 +0900

    refactor: enum에 description 필드 추가

--- src/main/java/com/bureureung/fo/domain/user/entity/UserGrade.java
+++ src/main/java/com/bureureung/fo/domain/user/entity/UserGrade.java
@@ -1,5 +1,15 @@
 package com.bureureung.fo.domain.user.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum UserGrade {
-    BRONZE, SILVER, GOLD, VIP;
+    BRONZE("브론즈"),
+    SILVER("실버"),
+    GOLD("골드"),
+    VIP("VIP");
+
+    private final String description;
 }
