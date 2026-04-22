c4231baa54bd3a8c2be893caec0ecdf1c06d53d2
techhan <devhan9117@gmail.com>
2026-04-22 17:30:24 +0900

    fix: @Id 어노테이션 패키지 변경

--- src/main/java/com/bureureung/fo/domain/auth/entity/RefreshToken.java
+++ src/main/java/com/bureureung/fo/domain/auth/entity/RefreshToken.java
@@ -1,7 +1,7 @@
 package com.bureureung.fo.domain.auth.entity;
 
-import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
 @Getter
